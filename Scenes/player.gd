d9bfd0d2627fdc5dc169ec88f7061637ab3f35b4
Luuqe <48174232+Luuqe@users.noreply.github.com>
2024-11-02 14:15:18 +0000

    Changed
    Changed camera and jump physics (broken)

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody3D
 # Downward acceleration when in the air, in meters per second squared
 @export var fall_acceleration = 75
 
+@export var jump_impulse = 20
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -19,8 +21,9 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
-	if Input.is_action_pressed("jump"):
-		direction.y += 1
+		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
@@ -32,6 +35,6 @@ func _physics_process(delta):
 	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-	
+			
 	velocity = target_velocity
 	move_and_slide()
