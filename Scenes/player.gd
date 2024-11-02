5799e1df00f9b2b15707d1f9e50b5140012b2fe4
towameister <tobiasf1404@gmail.com>
2024-11-02 14:29:04 +0100

    basic movement + static camera for testing

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -13,12 +13,14 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("move_back"):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+	if Input.is_action_pressed("jump"):
+		direction.y += 1
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
@@ -26,6 +28,7 @@ func _physics_process(delta):
 		
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
+	target_velocity.y = direction.y * speed
 	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
