cb194ae4d4f76143e7a70c538f58887fc84f5afd
Luuqe <48174232+Luuqe@users.noreply.github.com>
2024-11-03 16:57:29 +0000

    Changed girl

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 # Downward acceleration when in the air, in meters per second squared
 @export var fall_acceleration = 75
 @export var mouse_delta = Vector2()
-@export var sens = 0.01
+@export var sens = 0.001
 @export var jump_impulse = 1.5
 @export var dash_impulse = 3
 @onready var camera_pivot = $CameraPivot
@@ -20,13 +20,13 @@ var target_velocity = Vector3.ZERO
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+	if Input.is_action_pressed("move_left"):
+		direction.x += 1
 	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z += 1
 		
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		target_velocity.y = jump_impulse * speed
@@ -40,6 +40,7 @@ func _physics_process(delta):
 		target_velocity.x = orientation.x * speed
 		target_velocity.z = orientation.z * speed
 		dashCount = 0
+		
 	if not is_on_floor() and Input.is_action_just_pressed("jump") and Input.is_action_pressed("move_forward") and dashCount < maxDashes:
 		target_velocity.z = orientation.z * speed * dash_impulse 
 		dashCount += 1
@@ -66,7 +67,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		mouse_delta = event.relative
 		rotate_y(-event.relative.x * sens)
-		camera_pivot.rotate_x(-event.relative.y * sens)
+		camera_pivot.rotate_x(event.relative.y * sens)
 		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, deg_to_rad(-90), deg_to_rad(15))
 		
 func _process(delta) -> void:
