cbdd5ae106ecc7c8afc13cdaa2fec19a38d3a903
towameister <tobiasf1404@gmail.com>
2024-11-03 12:48:17 +0100

    update player gd, merge dash with new camera

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -7,32 +7,53 @@ extends CharacterBody3D
 @export var fall_acceleration = 75
 @export var mouse_delta = Vector2()
 @export var sens = 0.01
-@export var jump_impulse = 40
+@export var jump_impulse = 1.5
+@export var dash_impulse = 3
 @onready var camera_pivot = $CameraPivot
-@export var rot_x = 0
-@export var rot_y = 0
+var dashCount = 0
+var maxDashes = 3
+
 
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	var input = Vector3()
+	var direction = Vector3.ZERO
 	if Input.is_action_pressed("move_right"):
-		input.x += 1
+		direction.x += 1
 	if Input.is_action_pressed("move_left"):
-		input.x -= 1
+		direction.x -= 1
 	if Input.is_action_pressed("move_back"):
-		input.z += 1
+		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
-		input.z -= 1
+		direction.z -= 1
 		
 	if is_on_floor() and Input.is_action_pressed("jump"):
-		target_velocity.y = jump_impulse
-	var direction = Vector3()
-	if input != Vector3.ZERO:
-		input = input.normalized()
-		direction = (transform.basis.z * input.z + transform.basis.x * input.x)	 #how
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+		target_velocity.y = jump_impulse * speed
+		
+	var orientation = Vector3()
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		orientation = (transform.basis.z * direction.z + transform.basis.x * direction.x)	 #how
+		
+	if is_on_floor():
+		target_velocity.x = orientation.x * speed
+		target_velocity.z = orientation.z * speed
+		dashCount = 0
+	if not is_on_floor() and Input.is_action_just_pressed("jump") and Input.is_action_pressed("move_forward") and dashCount < maxDashes:
+		target_velocity.z = orientation.z * speed * dash_impulse 
+		dashCount += 1
+		
+	if not is_on_floor() and Input.is_action_just_pressed("jump") and Input.is_action_pressed("move_left") and dashCount < maxDashes:
+		target_velocity.x = orientation.x * speed * dash_impulse 
+		dashCount += 1
+		
+	if not is_on_floor() and Input.is_action_just_pressed("jump") and Input.is_action_pressed("move_back") and dashCount < maxDashes:
+		target_velocity.z = orientation.z * speed * dash_impulse 
+		dashCount += 1
+		
+	if not is_on_floor() and Input.is_action_just_pressed("jump") and Input.is_action_pressed("move_right") and dashCount < maxDashes:
+		target_velocity.x = orientation.x * speed * dash_impulse 
+		dashCount += 1
 	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
